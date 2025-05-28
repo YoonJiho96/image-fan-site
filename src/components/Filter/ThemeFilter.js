4629e45c570488ea3630cb900d3b26acdd67ec3c
윤지호 <hoyunji@naver.com>
2025-05-29 03:29:58 +0900

    feat: 일러스트 필터 추가

--- src/components/Filter/ThemeFilter.js
+++ src/components/Filter/ThemeFilter.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import FilterSelectBox from './FilterSelectBox';
+import themeData from './filter-theme.json';
 
 const ThemeFilter = () => {
   // 테마 필터
@@ -8,54 +9,29 @@ const ThemeFilter = () => {
   const [rare, setRare] = useState([]);
   const [unique, setUnique] = useState([]);
 
-  const illustrationOptions = [
-    { value: 'illust1', label: '일러압1' },
-    { value: 'illust2', label: '일러압2' },
-  ];
-
-  const swimOptions = [
-    { value: '2017', label: '해변의 랩소디(2017)' },
-    { value: '2018', label: '선샤인 웨이브(2018)' },
-  ];
-
-  const rareOptions = [
-    { value: '01', label: '암흑의 광휘' },
-    { value: '02', label: '사이버네틱' },
-    { value: '03', label: '지고의 날개' },
-    { value: '04', label: '칠흑의 날개' },
-    { value: '05', label: '드래고니안' },
-  ]
-
-  const uniqueOptions = [
-    { value: '01', label: '샤이닝 스타' },
-    { value: '02', label: '솔로몬의 예복' },
-    { value: '03', label: '휠 오브 트리니티' },
-    { value: '04', label: '황천의 사자' },
-  ]
-
   return (
     <div>
       <FilterSelectBox
         label="일러스트 아바타"
-        options={illustrationOptions}
+        options={themeData.일러스트.options}
         selectedOptions={illust}
         onChange={setIllust}
       />
       <FilterSelectBox
         label="수영복"
-        options={swimOptions}
+        options={themeData.수영복.options}
         selectedOptions={swim}
         onChange={setSwim}
       />
       <FilterSelectBox
         label="레어 아바타"
-        options={rareOptions}
+        options={themeData.레어.options}
         selectedOptions={rare}
         onChange={setRare}
       />
       <FilterSelectBox
         label="유니크 아바타"
-        options={uniqueOptions}
+        options={themeData.유니크.options}
         selectedOptions={unique}
         onChange={setUnique}
       />
