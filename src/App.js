60298a3285ed0d3ccb975b9bef2655ab65d54f32
윤지호 <hoyunji@naver.com>
2025-06-08 01:10:55 +0900

    feat: 메인 페이지 라우트 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import IllustrationPage from './page/IllustrationPage';
+import MainPage from './page/MainPage';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <main className="main-content">
           <Routes>
+            <Route path="/" element={<MainPage />} />
             <Route path="/gallery" element={<IllustrationPage />} />
           </Routes>
         </main>
